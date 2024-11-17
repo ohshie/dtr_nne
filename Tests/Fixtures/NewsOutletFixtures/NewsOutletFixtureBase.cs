39729938919bdf46e3ce6026a202086865186267
stas <@ohshie>
2024-11-17 11:23:52 +0100

    NFC005: fixes to how xunit process my fixtures. Slightly better approach for me now

---
+++ Tests/Fixtures/NewsOutletFixtures/NewsOutletFixtureBase.cs
@@ -0,0 +1,60 @@
+using dtr_nne.Domain.Entities;
+
+namespace Tests.Fixtures.NewsOutletFixtures;
+
+public static class NewsOutletFixtureBase
+{
+    public static readonly List<List<NewsOutlet>> Outlets = 
+    [
+        [
+            new NewsOutlet
+                {
+                    Id = Faker.RandomNumber.Next(600),
+                    Name = "arkeonews.net",
+                    Website = new Uri("https://arkeonews.net/"),
+                    MainPagePassword = "{ \"links\": \"li div.left a @href\" }",
+                    NewsPassword =
+                        "{ \"header\": \"h1.article-title\", \"body\": \"div.clearfix > p\", \"images\": \"img.attachment-post-thumbnail @src\", \"copyright\": \"\", \"source\": \"\" }",
+                    AlwaysJs = Faker.Boolean.Random(),
+                    InUse = Faker.Boolean.Random()
+                }
+        ], 
+        [
+            new NewsOutlet
+                {
+                    Id = Faker.RandomNumber.Next(600),
+                    Name = "arkeonews.net",
+                    Website = new Uri("https://arkeonews.net/"),
+                    MainPagePassword = "{ \"links\": \"li div.left a @href\" }",
+                    NewsPassword =
+                        "{ \"header\": \"h1.article-title\", \"body\": \"div.clearfix > p\", \"images\": \"img.attachment-post-thumbnail @src\", \"copyright\": \"\", \"source\": \"\" }",
+                    AlwaysJs = Faker.Boolean.Random(),
+                    InUse = Faker.Boolean.Random()
+                },
+
+            new NewsOutlet
+                {
+                    Id = Faker.RandomNumber.Next(600),
+                    Name = "eurekalert.org",
+                    Website = new Uri("https://eurekalert.org/"),
+                    MainPagePassword = "{ \"links\": \"article.trending a @href\" }",
+                    NewsPassword =
+                        "{ \"header\": \"h1.page_title\", \"body\": \"div.entry > p\", \"images\": \"div.img-wrapper img @src\", \"copyright\": \"p.credit\", \"source\": \"div.well a @href\" }",
+                    AlwaysJs = Faker.Boolean.Random(),
+                    InUse = Faker.Boolean.Random()
+                },
+
+            new NewsOutlet
+                {
+                    Id = Faker.RandomNumber.Next(600),
+                    Name = "techxplore.com/sort/date/12h/",
+                    Website = new Uri("https://techxplore.com/sort/date/12h/"),
+                    MainPagePassword = "{ \"links\": \"a.news-link @href\" }",
+                    NewsPassword =
+                        "{n  \"header\": \"h1.text-extra-large\",n  \"body\": \".article-main > p:not(.article-main__more)\",n  \"images\": \"figure.article-img img @src\",n  \"copyright\": \"figure.article-img figcaption\",n  \"source\": \"p.article-main__note a @href\"n}",
+                    AlwaysJs = Faker.Boolean.Random(),
+                    InUse = Faker.Boolean.Random()
+                }
+        ]
+    ];
+}
