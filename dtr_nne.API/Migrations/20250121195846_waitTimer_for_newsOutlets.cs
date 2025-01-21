fe133fdd7df53cd4758cee602930979748a4c1c0
stas <@ohshie>
2025-01-21 21:50:06 +0100

    nfc008: moving migrations to startup project

---
+++ dtr_nne.API/Migrations/20250121195846_waitTimer_for_newsOutlets.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dtr_nne.Migrations
+{
+    /// <inheritdoc />
+    public partial class waitTimer_for_newsOutlets : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WaitTimer",
+                table: "NewsOutlets",
+                type: "TEXT",
+                maxLength: 10,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WaitTimer",
+                table: "NewsOutlets");
+        }
+    }
+}
